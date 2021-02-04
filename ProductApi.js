8e28cad31104ec0e5070469abb63de15f7228355
Dharaneeshwaran97 <58076976+Dharaneeshwaran97@users.noreply.github.com>
2021-02-04 15:40:36 +0530

    auth API included changes

--- ProductApi.js
+++ ProductApi.js
@@ -15,11 +15,7 @@ class ProductAPI {
         const url = "https://shoppingapp-mock.herokuapp.com/api/brands";
         return axios.get(url);
     }
-    // get all users
-    async getUsers() {
-        const url = "http://shoppingapp-mock.herokuapp.com/api/users";
-        return axios.get(url);
-    }
+   
     //sorting ascending order using price value
     sorting(value1, value2) {
         if (value1.price < value2.price) {
@@ -28,30 +24,6 @@ class ProductAPI {
             return 1;
         } else {
             return 0;
-        }
-    }
-
-    // to check isValid user or not 
-    async isValidUser(loginDetails) {
-        let userDetails = await this.getUsers();
-        let users = userDetails.data;
-
-        let loginUser = users.find(p => p.email == loginDetails.email && p.password == loginDetails.password);
-        if (!loginUser) {
-            throw new Error("Invalid user details");
-        } else {
-            return loginUser
-        }
-    }
-    // to check success login or not
-    async login(loginDetails) {
-        try {
-            return await this.isValidUser(loginDetails);
-
-        } catch (err) {
-            console.log(err.message);
-
-            return err;
         }
     }
 
--- searchProduct.js
+++ searchProduct.js
@@ -1,25 +1,36 @@
+// import { AuthAPI } from "./authApi";
+
 const { ProductAPI } = require("./ProductApi");
+
+const { AuthAPI } = require("./authApi");
 var filters = { brandName: ["Samsung", "Apple", "RealMe"], ram: [4.5, 3], price: { min: 1000, max: 10000 }, sortBy: 'price' };
 
 var userLogin = { email: "admin@gmail.com", password: "pass123" };
 const productAPI = new ProductAPI();
 
+//to get all product details
 productAPI.getAllProducts().then(response => {
     return response.data;
 
 });
+
+//to get all brandname in products
 productAPI.getAllBrandName().then(response => {
     return response.data;
 })
 
-productAPI.login(userLogin).then(response => {
-    return response;
-
-})
-
+// search user input based fetch tha products
 productAPI.searchProducts(filters).then(response => {
     return response;
 })
+
+// check valid user or not
+const authAPI = new AuthAPI;
+authAPI.login(userLogin).then(response => {
+    return response;
+
+});
+
 // let returnValues = productAPI.getAllProducts();
 
 // let returnValues = await productAPI.getAllProducts();
